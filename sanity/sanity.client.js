88fb794253b1679c08aa9b7e3ddcd05885ef0fbf
vhutshilo <mavhandu.vb@gmail.com>
2023-08-31 18:06:47 +0200

    add

--- sanity/sanity.client.js
+++ sanity/sanity.client.js
@@ -3,7 +3,7 @@ import { createClient } from "next-sanity";
 const client = createClient({
   projectId: "pbmry4yo",
   dataset: "production",
-  apiVersion: "2023-07-16",
+  apiVersion: "2023-08-25",
   useCdn: false
 });
 
